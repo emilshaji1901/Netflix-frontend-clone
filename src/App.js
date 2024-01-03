bdb64a8c9b3cc73f779a7b60b3246a2ccff35c87
Emil Shaji <emilshaji2001>
2024-01-03 17:45:18 +0530

    Netflix frontend clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
+import Roww from './components/Roww';
+import requests from './requests';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <Nav/>
+
+        <Banner fetchUrl={requests.fetchNetflixOriginals}/>
+
+      <Roww title="Trending Movies" fetchUrl={requests.fetchTrending}/>
+
+      <Roww title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+
+<Roww isPoster={true} title="NetflixOriginals" fetchUrl={requests.fetchNetflixOriginals}/>
+
+      <Roww title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+
+      <Roww title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+
+      <Roww title="Horrer Movies" fetchUrl={requests.fetchHorrorMovies}/>
+
+      <Roww title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+
+      <Roww title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+
     </div>
   );
 }
